d0f772fa5a9e293191ae5ccdf1d1b299c4e729ee
Koen van Erp <vanerpkoen@gmail.com>
2018-10-31 15:06:38 +0100

    Not too bad

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [
+  {
+    name: "Mos Burger",
+    address: "Meguro",
+    category: "japanese"
+  },
+  {
+    name: "Some italian",
+    address: "Little Italy",
+    category: "italian"
+  },
+  {
+    name: "Little Paris",
+    address: "Kagurazaka",
+    category: "french"
+  },
+  {
+    name: "Great Burger",
+    address: "Harajuku",
+    category: "japanese"
+  },
+  {
+    name: "Little China",
+    address: "Yokohama",
+    category: "chinese"
+  }
+
+]
+
+Restaurant.create!(restaurants)
